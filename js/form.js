a9aac13e5e03ee22df30221c420c84adce824d36
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2022-10-07 13:48:01 +0200

    added form tag in the header

--- js/form.js
+++ js/form.js
@@ -1,6 +1,64 @@
 const form = document.querySelector("form.container")
+const firstName = document.querySelector("#firstName");
+const firstNameError = document.querySelector("#firstNameError");
+const lastName = document.querySelector("#lastName");
+const lastNameError = document.querySelector("#lastNameError");
+const email = document.querySelector("#emailaddress");
+const emailError = document.querySelector("#emailError");
+const cc = document.querySelector("#cc");
+const ccError = document.querySelector("#ccError");
+const cvv = document.querySelector("#cvv");
+const cvvError = document.querySelector("#cvvError");
+
 
 form.onsubmit = function() {
     event.preventDefault();
 
+    if (checkLength(firstName.value, 0) === true) {
+        firstNameError.style.display = "none";
+    } else {
+        firstNameError.style.display = "block";
+    }
+
+    if (checkLength(lastName.value, 3) === true) {
+        lastNameError.style.display = "none";
+    } else {
+        lastNameError.style.display = "block";
+    }
+
+    if (validateEmail(email.value) === true) {
+        emailError.style.display = "none";
+    } else {
+        emailError.style.display = "block";
+    }
+
+    if (checkLength(cc.value, 3) === true) {
+        ccError.style.display = "none";
+    } else {
+        ccError.style.display = "block";
+    }
+
+    if (validateEmail(cvv.value) === true) {
+        cvvError.style.display = "none";
+    } else {
+        cvvError.style.display = "block";
+    }
+
+}
+
+
+form.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+    if (value.trim().length > len) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
 }
